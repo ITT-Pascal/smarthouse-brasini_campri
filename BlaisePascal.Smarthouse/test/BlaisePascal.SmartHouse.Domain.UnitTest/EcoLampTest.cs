a4503b38c301bdbcb48d51ba885f1b769738368c
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2025-11-05 10:33:05 +0100

    Implemented EcoLamp tests 5/11

--- BlaisePascal.Smarthouse/test/BlaisePascal.SmartHouse.Domain.UnitTest/EcoLampTest.cs
+++ BlaisePascal.Smarthouse/test/BlaisePascal.SmartHouse.Domain.UnitTest/EcoLampTest.cs
@@ -9,6 +9,20 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTest
     public class EcoLampTest
     {
         [Fact]
+        public void constructor_WhenLampIsCreated_IsOnIsFalse()
+        {
+            EcoLamp lamp = new EcoLamp();
+            Assert.False(lamp.IsOn);
+        }
+
+        [Fact]
+        public void constructor_WhenLampIsCreated_BrightnessIsHisMaxValue()
+        {
+            EcoLamp lamp = new EcoLamp();
+            Assert.Equal(5, lamp.Brightness);
+        }
+
+        [Fact]
         public void switchIsOnOff_WhenIsOnIsFalse_ItChangesItToTrue()
         {
             EcoLamp lamp = new EcoLamp();
@@ -26,9 +40,66 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTest
         }
 
 
+        [Fact]
+        public void increaseBrightness_WhenBrightnessIsMax_ItDoesNotIncrease()
+        {
+            EcoLamp lamp = new EcoLamp();
+            lamp.IncreaseBrightness();
+            Assert.Equal(5, lamp.Brightness);
+        }
+
+        [Fact]
+        public void increaseBrightness_WhenBrightnessIsLessThanMax_ItIncreasesByOne()
+        {
+            EcoLamp lamp = new EcoLamp();
+            lamp.DecreaseBrightness();
+            lamp.IncreaseBrightness();
+            Assert.Equal(5, lamp.Brightness);
+        }
+
+        [Fact]
+        public void decreaseBrightness_WhenBrightnessIsMoreThanMin_ItDecreasesByOne()
+        {
+            EcoLamp lamp = new EcoLamp();
+            lamp.DecreaseBrightness();
+            Assert.Equal(4, lamp.Brightness);
+        }
+
+        [Fact]
+        public void decreaseBrightness_WhenBrightnessIsMin_ItDoesNotDecrease()
+        {
+            EcoLamp lamp = new EcoLamp();
+            for(int i = 0; i < 5; i++)
+            {
+                lamp.DecreaseBrightness();
+            }
+
+            Assert.Equal(1, lamp.Brightness);
+        }
+
+        [Fact]
+        public void changeBrightness_WhenNewBrightnessIsMoreThanMax_ThrowError()
+        {
+            EcoLamp lamp = new EcoLamp();
+            Assert.Throws<ArgumentOutOfRangeException>(() => lamp.ChangeBrightness(6));
+        }
+
+        [Fact]
+        public void changeBrightness_WhenNewBrightnessIsLessThanMin_ThrowError()
+        {
+            EcoLamp lamp = new EcoLamp();
+            Assert.Throws<ArgumentOutOfRangeException>(() => lamp.ChangeBrightness(0));
+        }
+
+        [Fact]
+        public void changeBrightness_WhenNewBrightnessIsInsideTheRange_AssignBightnessCorrectly()
+        {
+            EcoLamp lamp = new EcoLamp();
+            lamp.ChangeBrightness(3);
+            Assert.Equal(3, lamp.Brightness);
+        }
 
 
-       
 
     }
 }
