4baf63dd7988d3fb32c16adfb46d1c46b05453c6
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2025-11-07 09:24:52 +0100

    Added class LampsRow and part of LampsRow Test 07/11

--- BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Domain/LampsRow.cs
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Domain/LampsRow.cs
@@ -8,6 +8,88 @@ namespace BlaisePascal.SmartHouse.Domain
 {
     public class LampsRow
     {
-        
+        //Properties
+        public List<LampModel> Lamps { get; private set; }
+
+        //Constructor
+        public LampsRow()
+        {
+            Lamps = new List<LampModel>();
+        }
+
+        public LampsRow(int numLamp)
+        {
+            Lamps = new List<LampModel>();
+            for (int i = 0; i < numLamp; i++)
+            {
+
+                Lamps.Add(new Lamp());
+            }
+        }
+
+        //Methods
+        public void AddLamp(LampModel lamp)
+        {
+            Lamps.Add(lamp);
+        }
+
+
+        public void SwitchAllOn()
+        {
+            for (int i = 0; i < Lamps.Count; i++)
+            {
+                if (!Lamps[i].IsOn)
+                    Lamps[i].SwitchOnOff();
+
+            }
+        }
+
+        public void SwitchAllOff()
+        {
+            for (int i = 0; i > Lamps.Count; i++)
+            {
+                if (Lamps[i].IsOn)
+                    Lamps[i].SwitchOnOff();
+            }
+        }
+
+        public void SwitchOneLampOnOff(Guid id)
+        {
+            for (int i = 0; i < Lamps.Count; i++)
+            {
+                if (Lamps[i].ID == id)
+                {
+                    Lamps[i].SwitchOnOff();
+                }
+            }
+        }
+
+        public void IncreaseAllBrightness()
+        {
+            for (int i = 0; i < Lamps.Count; i++)
+            {
+                Lamps[i].IncreaseBrightness();
+            }
+        }
+
+        public void DecreaseeAllBrightness()
+        {
+            for (int i = 0; i < Lamps.Count; i++)
+            {
+                Lamps[i].DecreaseBrightness();
+            }
+        }
+
+        public void ChangeOneLampBrightness(Guid id, int newBrightness)
+        {
+            for (int i = 0; i < Lamps.Count; i++)
+            {
+                if (Lamps[i].ID == id)
+                    Lamps[i].ChangeBrightness(newBrightness);
+            }
+        }
+
+
     }
 }
+
---
+++ BlaisePascal.Smarthouse/test/BlaisePascal.SmartHouse.Domain.UnitTest/LampsRowTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.UnitTest
+{
+    public class LampsRowTest
+    {
+        [Fact]
+        public void Constructor_WhenCreateNewLampsRow_ItIsEmpty()
+        {
+            //Arrange & Act
+            LampsRow lampsRow = new LampsRow();
+            //Assert
+            Assert.Empty(lampsRow.Lamps);
+        }
+
+        [Fact]
+        public void Constructor_WhenCreateNewLampsRowWith4Lamps_ItHas4NormalLamps()
+        {
+            //Arrange & Act
+            LampsRow lampsrow = new LampsRow(4);
+            //Assert
+            Assert.Equal(4, lampsrow.Lamps.Count);
+        }
+
+        [Fact]
+        public void AddLamp_WhenANewEcoLampIsAdded_ItIsAddedInTheFirstFreePosition()
+        {
+            //Arrange
+            LampsRow lampsrow = new LampsRow();
+            //Act
+            lampsrow.AddLamp(new EcoLamp());
+            //Assert
+            Assert.Equal(1, lampsrow.Lamps.Count);
+        }
+
+        [Fact]
+        public void SwitchAllOn_AfterTurningAllOn_EveryLampIsOn()
+        {
+            //Arrange
+            LampsRow lampsrow = new LampsRow(5);
+            //Act
+            lampsrow.SwitchAllOn();
+            //Assert
+            for (int i = 0; i < lampsrow.Lamps.Count; i++)
+            {
+                Assert.True(lampsrow.Lamps[i].IsOn);
+            }
+        }
+    }
+}
