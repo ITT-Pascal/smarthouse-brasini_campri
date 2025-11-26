5f36945895828a67ad468fb6a7646e142c9a15d0
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2025-11-26 10:55:05 +0100

    Finished AirConditioner Tests

--- BlaisePascal.Smarthouse/test/BlaisePascal.SmartHouse.Domain.UnitTest/AirConditionerTests.cs
+++ BlaisePascal.Smarthouse/test/BlaisePascal.SmartHouse.Domain.UnitTest/AirConditionerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -133,6 +134,253 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTest
             Assert.Equal(AirMode.Fan, cond.Mode);
         }
 
+        [Fact]
+        public void SetDryMode_WhenConditionerIsOff_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cond.SetDryMode());
+        }
 
+        [Fact]
+        public void SetDryMode_WhenModeIsAlreadyDry_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDryMode();
+            //Assert
+            Assert.Throws<Exception>(() => cond.SetDryMode());
+        }
+
+        [Fact]
+        public void SetDryMode_WhenModeIsNotDry_SetIt()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDryMode();
+            //Assert
+            Assert.Equal(AirMode.Dry, cond.Mode);
+        }
+
+        [Fact]
+        public void SetMode_WhenConditionerIsOff_ThrowsError()
+        {
+            //Arrange 
+            AirConditioner cond = new AirConditioner();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cond.SetMode(AirMode.Normal));
+        }
+
+        [Fact]
+        public void SetMode_WhenModeIsAlreadyTheOneSelected_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            //Assert
+            Assert.Throws<ArgumentException>(() => cond.SetMode(AirMode.Normal));
+        }
+
+        [Fact]
+        public void SetMode_WhenModeIsNotTheOneSelected_ChangesIt()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetMode(AirMode.Fan);
+            //Assert
+            Assert.Equal(AirMode.Fan, cond.Mode);
+        }
+
+        [Fact]
+        public void SetDegrees_WhenConditionerIsOff_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cond.SetDegrees(24));
+        }
+
+        [Fact]
+        public void SetDegrees_WhenDegreesAreUnderMinValue_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => cond.SetDegrees(15));
+        }
+
+        [Fact]
+        public void SetDegrees_WhenDegreesAreOverMaxValue_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => cond.SetDegrees(28));
+        }
+
+        [Fact]
+        public void SetDegrees_WhenModeIsDry_CannotChangeDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetMode(AirMode.Dry);
+            //Assert
+            Assert.Throws<Exception>(() => cond.SetDegrees(20));
+        }
+
+        [Fact]
+        public void SetDegrees_WhenModeIsFan_CannotChangeDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetMode(AirMode.Fan);
+            //Assert
+            Assert.Throws<Exception>(() => cond.SetDegrees(20));
+        }
+
+        [Fact]
+        public void SetDegrees_WhenModeIsNormalAndDegreesAreInRange_SetToNewDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDegrees(20);
+            //Assert
+            Assert.Equal(20, cond.Degrees);
+        }
+
+
+        [Fact]
+        public void IncreaseDegrees_WhenConditionerIsOff_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cond.IncreaseDegrees());
+        }
+
+        [Fact]
+        public void IncreaseDegrees_WhenModeIsDry_CannotIncreaseDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDryMode();
+            //Assert
+            Assert.Throws<Exception>(() => cond.IncreaseDegrees());
+        }
+
+        [Fact]
+        public void IncreaseDegrees_WhenModeIsFan_CannotIncreaseDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetFanMode();
+            //Assert
+            Assert.Throws<Exception>(() => cond.IncreaseDegrees());
+        }
+
+        [Fact]
+        public void IncreaseDegrees_WhenDegreesAreNotMax_IncreaseByHalfDegree()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.IncreaseDegrees();
+            //Assert
+            Assert.Equal(20.5, cond.Degrees);
+        }
+
+        [Fact]
+        public void IncreaseDegrees_WhenDegreesAreMaxOrGetToMax_SetToMax()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDegrees(27);
+            cond.IncreaseDegrees();
+            //Assert
+            Assert.Equal(27.0, cond.Degrees);
+        }
+
+        [Fact]
+        public void DecreaseDegrees_WhenConditionerIsOff_ThrowsError()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cond.DecreaseDegrees());
+        }
+
+        [Fact]
+        public void DecreaseDegrees_WhenModeIsDry_CannotDecreaseDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDryMode();
+            //Assert
+            Assert.Throws<Exception>(() => cond.DecreaseDegrees());
+        }
+
+        [Fact]
+        public void DecreaseDegrees_WhenModeIsFan_CannotDecreaseDegrees()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetFanMode();
+            //Assert
+            Assert.Throws<Exception>(() => cond.DecreaseDegrees());
+        }
+
+        [Fact]
+        public void DecreaseDegrees_WhenDegreesAreNotMax_DecraseByHalfDegree()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.DecreaseDegrees();
+            //Assert
+            Assert.Equal(19.5, cond.Degrees);
+        }
+
+        [Fact]
+        public void DecreaseDegrees_WhenDegreesAreMinOrGetToMin_SetToMin()
+        {
+            //Arrange
+            AirConditioner cond = new AirConditioner();
+            //Act
+            cond.SwitchOn();
+            cond.SetDegrees(16);
+            cond.DecreaseDegrees();
+            //Assert
+            Assert.Equal(16.0, cond.Degrees);
+        }
     }
 }
