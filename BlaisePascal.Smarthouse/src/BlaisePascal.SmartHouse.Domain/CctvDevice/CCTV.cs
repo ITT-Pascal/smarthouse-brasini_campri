23c668f3458a0c8367bfddaaa84f74c75e128cb3
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2025-12-04 16:33:12 +0100

    Added class CCTV and the relatives tests

---
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Domain/CctvDevice/CCTV.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.CctvDevice
+{
+    public class CCTV
+    {
+        //Properties
+
+        public string Name { get; private set; }
+        public Guid Id { get; private set; }
+        public DeviceStatus Status { get; private set; }
+        public CCTVMode Mode { get; private set; }
+        public DateTime LastModifiedAtUtc { get; private set; }
+        public DateTime CreatedAtUtf { get; private set; }
+        public bool isRecording { get; private set; }
+
+        //Constructor
+        public CCTV()
+        {
+            Id = Guid.NewGuid();
+            Status = DeviceStatus.Off;
+            Mode = CCTVMode.NoMode;
+            CreatedAtUtf = DateTime.UtcNow;
+            isRecording = false;
+        }
+        public CCTV(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+            Status = DeviceStatus.Off;
+            Mode = CCTVMode.NoMode;
+            CreatedAtUtf = DateTime.UtcNow;
+            isRecording = false;
+        }
+        public CCTV(string name, Guid id)
+        {
+            Name = name;
+            Id = id;
+            Status = DeviceStatus.Off;
+            Mode = CCTVMode.NoMode;
+            CreatedAtUtf = DateTime.UtcNow;
+            isRecording = false;
+        }
+
+        //Methods
+        private void OnValidator()
+        {
+            if (Status == DeviceStatus.Off)
+                throw new Exception("The device is off");
+        }
+
+
+
+        public void TurnOn()
+        {
+            if (Status == DeviceStatus.On)
+                throw new Exception("The CCTV is already on");
+
+            Status = DeviceStatus.On;
+            Mode = CCTVMode.Normal;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+
+        public void TurnOff()
+        {
+            if (Status == DeviceStatus.Off)
+                throw new Exception("The CCTV is already off");
+
+            Status = DeviceStatus.Off;
+            Mode = CCTVMode.NoMode;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+
+        public void SetNormalMode()
+        {
+            OnValidator();
+            if (Mode == CCTVMode.Normal)
+                throw new Exception("The mode is already normal");
+            Mode = CCTVMode.Normal;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+
+        public void SetNightMode()
+        {
+            OnValidator();
+            if (Mode == CCTVMode.Night)
+                throw new Exception("The mode is already night");
+            Mode = CCTVMode.Night;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+
+        public void SetMode(CCTVMode mode)
+        {
+            OnValidator();
+            if (Mode == mode)
+                throw new ArgumentException($"The mode is already {mode}");
+            Mode = mode;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+
+        public void StartRecording()
+        {
+            OnValidator();
+            if (isRecording)
+                throw new Exception("The cctv is already recording");
+            isRecording = true;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+
+        public void StopRecording()
+        {
+            OnValidator();
+            if (!isRecording)
+                throw new Exception("The cctv is not recording");
+            isRecording = false;
+            LastModifiedAtUtc = DateTime.UtcNow;
+        }
+    }
+}
---
+++ BlaisePascal.Smarthouse/test/BlaisePascal.SmartHouse.Domain.UnitTest/CCTVTests.cs
@@ -0,0 +1,232 @@
+﻿using BlaisePascal.SmartHouse.Domain.CctvDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.UnitTest
+{
+    public class CCTVTests
+    {
+        [Fact]
+        public void Constructor_AfterCreating_TheCctvIsOff()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Equal(DeviceStatus.Off, cctv.Status);
+            Assert.Equal(CCTVMode.NoMode, cctv.Mode);
+            Assert.False(cctv.isRecording);
+        }
+
+        [Fact]
+        public void TurnOn_WhenCctvIsOff_TurnItOn()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            //Assert
+            Assert.Equal(DeviceStatus.On, cctv.Status);
+        }
+
+        [Fact]
+        public void TurnOn_WhenCctvIsOn_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            //Assert
+            Assert.Throws<Exception>(() => cctv.TurnOn());
+        }
+
+
+        [Fact]
+        public void TurnOff_WhenCctvIsOn_TurnItOn()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.TurnOff();
+            //Assert
+            Assert.Equal(DeviceStatus.Off, cctv.Status);
+        }
+
+        [Fact]
+        public void TurnOff_WhenCctvIsOff_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cctv.TurnOff());
+        }
+
+        [Fact]
+        public void SetNormalMode_WhenCctvIsOff_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cctv.SetNormalMode());
+        }
+
+        [Fact]
+        public void SetNormalMode_WhenModeIsAlreadyNormal_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            //Assert
+            Assert.Throws<Exception>(() => cctv.SetNormalMode());
+        }
+
+        [Fact]
+        public void SetNormalMode_WhenModeNotNormal_SetItToNormal()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.SetNightMode();
+            cctv.SetNormalMode();
+            //Assert
+            Assert.Equal(CCTVMode.Normal, cctv.Mode);
+        }
+
+        [Fact]
+        public void SetNightMode_WhenCctvIsOff_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cctv.SetNightMode());
+        }
+
+        [Fact]
+        public void SetNightMode_WhenModeIsAlreadyNight_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.SetNightMode();
+            //Assert
+            Assert.Throws<Exception>(() => cctv.SetNightMode());
+        }
+
+        [Fact]
+        public void SetNightMode_WhenModeNotNight_SetItToNight()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.SetNightMode();
+            //Assert
+            Assert.Equal(CCTVMode.Night, cctv.Mode);
+        }
+
+        [Fact]
+        public void SetMode_WhenCctvIsOff_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cctv.SetMode(CCTVMode.Normal));
+        }
+
+        [Fact]
+        public void SetMode_WhenCctvIsOnTheModeChoosed_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            //Assert
+            Assert.Throws<ArgumentException>(() => cctv.SetMode(CCTVMode.Normal));
+        }
+
+        [Fact]
+        public void SetMode_WhenCctvIsNotOnTheModeChoosed_ChangeMode()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.SetMode(CCTVMode.Night);
+            //Assert
+            Assert.Equal(CCTVMode.Night, cctv.Mode);
+        }
+
+        [Fact]
+        public void StartRecording_WhenCctvIsOff_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cctv.StartRecording());
+        }
+
+        [Fact]
+        public void StartRecording_WhenCctvIsAlreadyRecording_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.StartRecording();
+            //Assert
+            Assert.Throws<Exception>(() => cctv.StartRecording());
+        }
+
+        [Fact]
+        public void StartRecording_WhenCctvIsNotRecording_StartTheRecording()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.StartRecording();
+            //Assert
+            Assert.True(cctv.isRecording);
+        }
+
+        [Fact]
+        public void StopRecording_WhenCctvIsOff_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act & Assert
+            Assert.Throws<Exception>(() => cctv.StopRecording());
+        }
+
+        [Fact]
+        public void StopRecording_WhenCctvIsNotRecording_ThrowError()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            //Assert
+            Assert.Throws<Exception>(() => cctv.StopRecording());
+        }
+
+        [Fact]
+        public void StopRecording_WhenCctvIsRecording_StopTheRecording()
+        {
+            //Arrange
+            CCTV cctv = new CCTV();
+            //Act
+            cctv.TurnOn();
+            cctv.StartRecording();
+            cctv.StopRecording();
+            //Assert
+            Assert.False(cctv.isRecording);
+        }
+    }
+}
