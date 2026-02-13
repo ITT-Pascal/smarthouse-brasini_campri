4556b38ccf113d2d6e9480369d6ca4af5906733d
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2026-02-13 09:49:50 +0100

    Added Application Layer

---
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Application/LuminousDevices/Lamp/Queries/GetLampByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using BlaisePascal.SmartHouse.Domain.LuminousDevices.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.LuminousDevices.Lamp.Queries
+{
+    public class GetLampByIdQuery
+    {
+        private readonly ILampRepository _lampRepository;
+
+        public GetLampByIdQuery(ILampRepository lampRepository)
+        {
+            _lampRepository = lampRepository;
+        }
+    }
+}
