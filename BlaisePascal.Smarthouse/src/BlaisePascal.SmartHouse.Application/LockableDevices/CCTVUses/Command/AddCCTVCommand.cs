63b1f907416bd7a7c71b75961104bb848200f92e
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2026-02-19 13:10:20 +0100

    Started CCTV uses

---
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Application/LockableDevices/CCTVUses/Command/AddCCTVCommand.cs
@@ -0,0 +1,25 @@
+﻿using BlaisePascal.SmartHouse.Domain.LockableDevices.CctvDevice;
+using BlaisePascal.SmartHouse.Domain.LockableDevices.CctvDevice.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.LockableDevices.CCTVUses.Command
+{
+    public class AddCCTVCommand
+    {
+        private readonly ICCTVRepsotitory _cctvRepository;
+
+        public AddCCTVCommand(ICCTVRepsotitory cctvRepository)
+        {
+            _cctvRepository = cctvRepository;
+        }
+
+        public void Execute(string name)
+        {
+            _cctvRepository.Add(new CCTV(name));
+        }
+    }
+}
