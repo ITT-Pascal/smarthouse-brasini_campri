71a05181124522b51618e0f97bd9eb5c6359424a
brasinimattia <mattia.brasini.stud@ispascalcomandini.it>
2026-02-19 13:49:21 +0100

    Finished Door Use Cases

---
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Application/LockableDevices/DoorUses/Queries/GetDoorByIdQuery.cs
@@ -0,0 +1,23 @@
+﻿using BlaisePascal.SmartHouse.Domain.LockableDevices.DoorDevice;
+using BlaisePascal.SmartHouse.Domain.LockableDevices.DoorDevice.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.LockableDevices.DoorUses.Queries
+{
+    public class GetDoorByIdQuery
+    {
+        private readonly IDoorRepository _doorRepository;
+        public GetDoorByIdQuery(IDoorRepository doorRepository)
+        {
+            _doorRepository = doorRepository;
+        }
+        public Door Execute(Guid id)
+        {
+            return _doorRepository.GetById(id);
+        }
+    }
+}
