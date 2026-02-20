abd8a175154f434e6f0e829c0187c9a91de4be9a
CampriNicola <nicola.campri.stud@ispascalcomandini.it>
2026-02-20 09:49:45 +0100

    Started CCTV mappers

---
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Application/Devices/Abstraction/Mappers/CCTVModeMapper.cs
@@ -0,0 +1,22 @@
+﻿using BlaisePascal.SmartHouse.Domain.LockableDevices.CctvDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Abstraction.Mappers
+{
+    public class CCTVModeMapper
+    {
+        public static string ToDto(CCTVMode mode)
+        {
+            return mode switch
+            {
+                CCTVMode.NoMode => "NOMODE",
+                CCTVMode.Night => "NIGHT",
+                CCTVMode.Normal => "NORMAL"
+            };
+        }
+    }
+}
