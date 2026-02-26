76b183ac927066b52bd4e98234b9c7e850279401
brasinimattia <mattia.brasini.stud@ispascalcomandini.it>
2026-02-26 11:46:24 +0100

    Added Door InMemory

---
+++ BlaisePascal.Smarthouse/src/BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lockable/Doors/InMemory/InMemoryDoorRepository.cs
@@ -0,0 +1,52 @@
+﻿
+using BlaisePascal.SmartHouse.Domain.LockableDevices.DoorDevice;
+using BlaisePascal.SmartHouse.Domain.LockableDevices.DoorDevice.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Infrastructure.Repositories.Devices.Lockable.Doors.InMemory
+{
+    public class InMemoryDoorRepository : IDoorRepository
+    {
+        private readonly List<Door> _doors;
+        public InMemoryDoorRepository()
+        {
+            _doors = new List<Door>()
+            {
+                new Door("Door1")
+            };
+        }
+        public List<Door> GetAll()
+        {
+            return _doors;
+        }
+
+        public Door GetById(Guid id)
+        {
+            return _doors.First(l => l.Id == id);
+        }
+
+        public void Add(Door door)
+        {
+            if (door == null)
+                throw new ArgumentNullException(nameof(door));
+            _doors.Add(door);
+        }
+
+        public void Remove(Guid id)
+        {
+            Door door = GetById(id);
+            if (door != null)
+                _doors.Remove(door);
+
+        }
+
+        public void Update(Door lamp)
+        {
+            //Todo: implement update logic
+        }
+    }
+}
